7b694a8e4ba5e7da6b2219f0800946ace936d906
Guadalupe <Guadalupe@192.168.1.11>
2018-04-16 22:26:26 -0600

    Mi aporte fue el "Hola Mundo"

---
+++ src/pruebaa2/Pruebaa2.java
@@ -0,0 +1,13 @@
+package pruebaa2;
+
+/**
+ *
+ * @author Guadalupe
+ */
+public class Pruebaa2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo");
+    }
+    
+}
