b1d5bada8065362e613102fbb638eb9adf416526
kenyaa <38260872+kenyaa@users.noreply.github.com>
2018-04-16 22:44:08 -0600

    Update Pruebaa2.java
    Escribí otro mensaje

--- src/pruebaa2/Pruebaa2.java
+++ src/pruebaa2/Pruebaa2.java
@@ -9,6 +9,7 @@ public class Pruebaa2 {
     public static void main(String[] args) {
         System.out.println("Hola Mundo");
         System.out.println("Hola Kenya");
+        System.out.println("Hola Otra Vez");
     }
     
 }
